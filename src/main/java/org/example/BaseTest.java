14c353d3e9663207473435c0666ca37f49bdca6e
alekuzmin <onfate@yandex.ru>
2022-02-11 22:13:32 +0300

    Add filters log

--- src/main/java/org/example/BaseTest.java
+++ src/main/java/org/example/BaseTest.java
@@ -2,8 +2,8 @@ package org.example;
 
 import io.restassured.RestAssured;
 import io.restassured.config.EncoderConfig;
-import io.restassured.response.Response;
-import io.restassured.response.ValidatableResponse;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
@@ -18,23 +18,11 @@ public class BaseTest {
         RestAssured.config().encoderConfig(EncoderConfig.encoderConfig().appendDefaultContentCharsetToContentTypeIfUndefined(false));
 
         given()
+                .filter(new RequestLoggingFilter())
+                .filter(new ResponseLoggingFilter())
                 .param("user", "123")
                 .when()
                 .get("https://a5a6ff47-6f34-4d17-9ad7-0c3b87ac07b5.mock.pstmn.io/auth/getToken")
-                .then()
-                .log()
-                .body();
-
-        //given().when().get("https://a5a6ff47-6f34-4d17-9ad7-0c3b87ac07b5.mock.pstmn.io/auth/getToken?user=123");
-
-        Response response = given()
-                .param("user", "123")
-                .when()
-                .get("https://a5a6ff47-6f34-4d17-9ad7-0c3b87ac07b5.mock.pstmn.io/auth/getToken");
-
-        System.out.println(response.asString());
-
-        response
                 .then()
                 .statusCode(200);
 
@@ -47,18 +35,17 @@ public class BaseTest {
                 " \"id\": " + userId + "" +
                 "\n }";
 
-        Response resp = given()
+        given()
+                .filter(new RequestLoggingFilter())
+                .filter(new ResponseLoggingFilter())
                 .baseUri("https://a5a6ff47-6f34-4d17-9ad7-0c3b87ac07b5.mock.pstmn.io/api/v1/client/orders")
                 .header("Content-Type", "application/json")
                 .body(json)
-                .post();
-
-        //System.out.println(resp.body().asString());
-
-        resp
+                .post()
                 .then()
                 .body("orders.id[0]", equalTo(100500))
                 .body("orders.name[1]", equalTo("eat"));
+
     }
 
     @Test
@@ -69,16 +56,15 @@ public class BaseTest {
                 " \"id\": " + userId + "" +
                 "\n }";
 
-        ValidatableResponse validatableResponse = given()
+        given()
+                .filter(new RequestLoggingFilter())
+                .filter(new ResponseLoggingFilter())
                 .baseUri("https://a5a6ff47-6f34-4d17-9ad7-0c3b87ac07b5.mock.pstmn.io/api/v1/client/orders")
                 .header("Content-Type", "application/json")
                 .body(json)
                 .post()
-                .then();
-
-        validatableResponse
-                .assertThat()
-                .body(matchesJsonSchemaInClasspath("schema.json"));
+                .then()
+                .body(matchesJsonSchemaInClasspath("schema.json"));;
 
     }
 }
